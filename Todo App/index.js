4b60186e246be15b5553d9936d4efc7e6ea1dc68
Amrit <109876062+ritzAmp@users.noreply.github.com>
2025-09-20 14:32:09 +0300

    Create index.js

---
+++ Todo App/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started in port ${PORT}`);
+});
